49322700d9ae78a321637c57063ac151df961290
Mayank <mayankgupta8900@gmail.com>
2021-11-25 12:21:22 +0530

    made array

--- CricInfoExtractor.js
+++ CricInfoExtractor.js
@@ -5,18 +5,53 @@ let excel = require("excel4node");
 let fs = require("fs");
 let jsdom = require("jsdom");
 let pdf = require("pdf-lib");
+const { matches } = require("lodash");
 
 let args = minimist(process.argv);
 //console.log(args.source);
 
 let axiosKaPromise = axios.get(args.source);
-axiosKaPromise.then(function(response){
+axiosKaPromise.then(function (response) {
   let html = response.data;
   //console.log(html);
   let dom = new jsdom.JSDOM(html);
   let document = dom.window.document;
-  let macthes=[];
-  let match= {};
-  let matchDiv = document.querySelectorAll("div.match-score-block");
+  let macthes = [];
+  let matchDivs = document.querySelectorAll("div.match-score-block");
   //console.log(matchDiv.length); total match mil gye yha !!
+  for (let i = 0; i < matchDivs.length; i++) {
+    let matchDiv = matchDivs[i];
+    let match = {
+      t1 = "",
+      t2 = "",
+      t1s = "",
+      t2s = "",
+      result = ""
+    };
+    let resultspan = matchDiv.querySelector("div.status>span");
+    match.result = resultspan.textContent;
+    let teamParas = matchDiv.querySelectorAll("div.name-detail>p.name");
+    match.t1 = teamParas[0].textContent;
+    match.t2 = teamParas[1].textContent;
+    let scoreSpans = matchDiv.querySelectorAll("div.score-detail>span");
+    if (scoreSpans == 2) {
+      match.t1s = scoreSpans[0].textContent;
+      match.t2s = scoreSpans[1].textContent;
+    }
+    else if (scoreSpans == 1) {
+      match.t1s = scoreSpans[0].textContent;
+      match.t2s = "";
+    }
+    else {
+      match.t1s = "";
+      match.t2s = "";
+    }
+    matches.push(match);
+    let matchesJSON = JSON.stringify(matches);
+    fs.writeFileSync("matches.json",matchesJSON,"utf-8");
+    let team = [];
+    for (let i=0;i<matches.length;i++){
+      
+    }
+  }
 })
